e98e81e498ace0a05f1cf62db5c040042b268014
ViplavReddy1 <138591936+ViplavReddy1@users.noreply.github.com>
2025-02-10 02:59:52 +0000

    datatypes

---
+++ practice/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; 
+/* treat all java script code according to the new version
+it is not necessary to mention it
+new version contains classes, modules etc.
+*/
+
+//alert("hello") alert gives error in nodejs, it runs in browser
+//use semicolon if the user want to write 2 statements in a single line
+//tc39.es for orginal documentation of the java script
+// for clarity use mdn website
+
+let name = "viplav"
+let age = 22
+let isGrad = false
+
+/* primitive datatype      range
+   number    => 2 to power 53
+   bigInt
+   string
+   boolean
+   null
+   undefined
+   symbol => used to find uniqueness
+   typeof(null)=> object
+   object datatype
+
+    */
+   console.log(typeof(name));
