419e366209017aa8be1b8a465d236200c3b0ba5b
ViplavReddy2002 <viplavbilla543@gmail.com>
2025-02-15 15:44:52 -0600

    strings

---
+++ practice/05_strings.js
@@ -0,0 +1,10 @@
+const fname = "viplav"
+const age = 22
+console.log(fname+age+"age");
+
+console.log(`Hello my name is ${fname} and my age is ${age}`)
+// This is called string interpolation
+// Best to use string interpolation
+//different type of declaration of string
+let name = new String("viplsv");
+console.log(name);
