6432d44806428d9599507e159a41045a262dd87e
ViplavReddy2002 <viplavbilla543@gmail.com>
2025-02-15 16:11:06 -0600

    numbers & maths

--- practice/06_numbers_and_maths.js
+++ practice/06_numbers_and_maths.js
@@ -4,3 +4,33 @@ const score = 100
 console.log(balance);
 console.log(score);
 
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//Maths object - contains maths related methods and functions
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.3));
+console.log(Math.max(4,3,5,6));
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+
+
